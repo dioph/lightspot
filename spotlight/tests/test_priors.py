1f82b9930cb99d5334d6dfa39cb92218d3d58cd7
dioph <dioph@protonmail.com>
2020-05-26 20:04:26 -0300

    tests config

---
+++ spotlight/tests/test_priors.py
@@ -0,0 +1,18 @@
+import unittest
+
+import numpy as np
+
+from spotlight.priors import Uniform
+
+
+class TestPrior(unittest.TestCase):
+    def test_uniform(self):
+        np.random.seed(42)
+        prior = Uniform()
+        samples = prior.sample(*np.random.random(30_000))
+        hist = np.histogram(samples, density=True)[0]
+        self.assertLess(np.max(np.abs(hist - np.ones(10))), 0.03)
+
+
+if __name__ == '__main__':
+    unittest.main()
