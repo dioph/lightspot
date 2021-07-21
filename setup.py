e9d015be0129dd908d43a6bd52969643f7266a97
dioph <dioph@protonmail.com>
2021-07-21 14:35:00 -0300

    update development infrastructure

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import re
 import sys
 
@@ -6,9 +9,7 @@ from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
 version = re.search(
-    '^__version__\\s*=\\s*"(.*)"',
-    open('lightspot/__init__.py').read(),
-    re.M
+    '^__version__\\s*=\\s*"(.*)"', open("src/lightspot/__init__.py").read(), re.M
 ).group(1)
 
 
@@ -16,30 +17,36 @@ class GetPybindInclude(object):
     """Helper class to determine the pybind11 include path
     The purpose of this class is to postpone importing pybind11
     until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
+    method can be invoked."""
 
     def __str__(self):
         import pybind11
+
         return pybind11.get_include()
 
 
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-extension = Extension(name="lightspot.macula",
-                      sources=["lightspot/macula.cpp"],
-                      include_dirs=[GetPybindInclude(), ],
-                      language="c++")
+extension = Extension(
+    name="lightspot.macula",
+    sources=["src/lightspot/macula.cpp"],
+    include_dirs=[
+        GetPybindInclude(),
+    ],
+    language="c++",
+)
 
 
 def has_flag(compiler, flagname):
     """Return a boolean indicating whether a flag name is supported on
     the specified compiler.
     """
-    import tempfile
     import os
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp', delete=False) as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
+    import tempfile
+
+    with tempfile.NamedTemporaryFile("w", suffix=".cpp", delete=False) as f:
+        f.write("int main (int argc, char **argv) { return 0; }")
         fname = f.name
     try:
         compiler.compile([fname], extra_postargs=[flagname])
@@ -57,43 +64,45 @@ def cpp_flag(compiler):
     """Return the -std=c++[11/14/17] compiler flag.
     The newer version is prefered over c++11 (when it is available).
     """
-    flags = ['-std=c++17', '-std=c++14', '-std=c++11']
+    flags = ["-std=c++17", "-std=c++14", "-std=c++11"]
 
     for flag in flags:
         if has_flag(compiler, flag):
             return flag
 
-    raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                       'is needed!')
+    raise RuntimeError("Unsupported compiler -- at least C++11 support " "is needed!")
 
 
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
+
     c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
+        "msvc": ["/EHsc"],
+        "unix": [],
     }
     l_opts = {
-        'msvc': [],
-        'unix': [],
+        "msvc": [],
+        "unix": [],
     }
 
-    if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        c_opts['unix'] += darwin_opts
-        l_opts['unix'] += darwin_opts
+    if sys.platform == "darwin":
+        darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
+        c_opts["unix"] += darwin_opts
+        l_opts["unix"] += darwin_opts
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
         opts = self.c_opts.get(ct, [])
         link_opts = self.l_opts.get(ct, [])
-        if ct == 'unix':
+        if ct == "unix":
             opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
+            if has_flag(self.compiler, "-fvisibility=hidden"):
+                opts.append("-fvisibility=hidden")
 
         for ext in self.extensions:
-            ext.define_macros = [('VERSION_INFO', '"{}"'.format(self.distribution.get_version()))]
+            ext.define_macros = [
+                ("VERSION_INFO", '"{}"'.format(self.distribution.get_version()))
+            ]
             ext.extra_compile_args = opts
             ext.extra_link_args = link_opts
         build_ext.build_extensions(self)
@@ -103,12 +112,13 @@ install_requires = [
     "dynesty >= 1.0",
     "numpy",
     "pybind11 >= 2.2",
-    "scipy >= 0.19"
+    "scipy >= 0.19",
+    "numba",
 ]
 
 extras_require = {
-    "docs": ["jupyter", "numpydoc", "pandoc", "sphinx_rtd_theme"],
-    "test": ["pytest", "flake8"]
+    "docs": ["jupyter", "numpydoc", "myst-nb<0.11", "sphinx_rtd_theme"],
+    "test": ["black==20.8b1", "flake8", "isort", "pytest", "pytest-cov", "tox"],
 }
 
 setup(
@@ -121,11 +131,12 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dioph/lightspot",
-    packages=["lightspot"],
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
     ext_modules=[extension],
     install_requires=install_requires,
     extras_require=extras_require,
-    cmdclass={'build_ext': BuildExt},
+    cmdclass={"build_ext": BuildExt},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
